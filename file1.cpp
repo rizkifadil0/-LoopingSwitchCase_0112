116711d2a04f26a7a7ca309a3d556998b65b8e04
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-03-18 19:02:34 +0700

    Perulangan For

---
+++ file1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    //pencacah naik
+    int i;
+    for (i = 0; i < 5; i++){
+        cout << "Nilai i " << i << " Lari Keliling Lapangan" << endl;
+        
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+
+    //pencacah turun
+    for (i = 5; i > 0; i--){
+        cout << "Nilai i " << i << " Lari Keliling Lapangan" << endl;
+        
+    }
+    cout << "Nilai i terakhir : " << i << endl;
+    
+}
