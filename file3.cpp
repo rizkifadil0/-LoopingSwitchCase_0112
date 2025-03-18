6e72719ae71381f538cdcc937afac3d3f6d1c0e3
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-03-18 20:20:57 +0700

    penambahan menu dan switch case

--- file3.cpp
+++ file3.cpp
@@ -13,6 +13,45 @@ int main(){
     int pilihan;
     float panjang, lebar;
     float jejari;
+    do
+    {
+        cout << "====Menu====" << endl;
+        cout << "1. Luas Persegi Panjang" << endl;
+        cout << "2. Luas Lingkaran" << endl;
+        cout << "3. Keluar" << endl;
+        
+        cout << "Plihan 1/2/3 = ";
+        cin >> pilihan;
 
+        switch (pilihan)
+        {
+            case 1:
+            cout << "Hitung Luas Persegi Panjang" << endl;
+            cout <<  "Masukkan Panjang : ";
+            cin >> panjang;
+            cout << "Masukkan Lebar : ";
+            cin >> lebar;
+            cout << "Luas Persegi Panjang = " << luaspersegipanjang(panjang, lebar) << endl;
+            break;
+
+            case 2: 
+            cout << "Hitung Luas Lingkaran" << endl;
+            cout << "Masukkan Jari Jari = " ;
+            cin >> jejari;
+            cout << "Luas Lingkaran = " << luaslingkaran(jejari) << endl;
+            break; 
+
+            case 3:
+            break;
+
+            default:
+            cout << "Pilihan Salah, pilihan harus di antara 1-3." << endl;
+            break;
+
+
+
+        }
+    } while (pilihan != 3);
+    
 
 }
