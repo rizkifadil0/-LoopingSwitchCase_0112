fcf9213f63c46512886706eff8c9a2e560a7dc71
rixkifadil@gmail.com <rixkifadil@gmail.com>
2025-03-18 20:19:50 +0700

    penyebutan fungsi

---
+++ file3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+float luaspersegipanjang(float a,float b){
+    return a * b;
+}
+
+float luaslingkaran(float a){
+    return 3.14 * a * a;
+}
+
+int main(){
+    int pilihan;
+    float panjang, lebar;
+    float jejari;
+
+
+}
